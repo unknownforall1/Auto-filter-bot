bb36f58cf4a4a7bd3ee9504a0283f98122a4b122
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-14 16:59:53 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/unknownforall1/Auto-filter-bot.git /Auto-filter-bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Auto-filter-bot
+fi
+cd /Auto-filter-bot
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
