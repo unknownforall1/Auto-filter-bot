6434cab5669a941488d6a3e8128fa88c4bdbc4b2
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-11-14 22:15:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
-NO_RESULTS_MSG_CHNL = environ.get('NO_RESULTS_MSG_CHNL',"")
+NO_RESULTS_MSG_CHNL = environ.get('NO_RESULTS_MSG_CHNL',"-1002026632746")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
